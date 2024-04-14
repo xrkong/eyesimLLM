82085d2f39b8263781bac28077ede73d7af4abf0
moebuta <wenxiao.zhang@research.uwa.edu.au>
2024-04-14 17:38:11 +0800

    initial completed pipeline

--- src/finder_llm.py
+++ src/finder_llm.py
@@ -9,6 +9,14 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 baseurl = "http://host.docker.internal:8000/queue_task/"
+# baseurl = "https://api.nlp-tlp.org/queue_task/"
+
+with open("llm/prompt/system.txt", 'r') as system_file:
+    system = system_file.read()
+
+with open("llm/prompt/functions.json", 'r') as tools_file:
+    tools = json.load(tools_file)
+
 
 SAFE = 200
 
@@ -39,6 +47,22 @@ def red_detector(img):
     return [True, max_col, max]
 
 
+def LLMQuery(req: LLMRequest, status: str) -> dict:
+
+
+    messages = [
+        {"role": "system", "content": system},
+        {"role": "user", "content": status}
+    ]
+
+    response = req.get_completion(messages=messages, tools=tools, tool_choice="auto", query_interval=1)
+    message_dict = ast.literal_eval(response['desc'])
+    command = message_dict['choices'][0]['message']['tool_calls'][0]['function']
+    logger.info(command)
+    return command
+
+
+
 def main():
     img = []
     l, f, r = 0, 0, 0
@@ -49,42 +73,38 @@ def main():
 
     req = LLMRequest(baseurl=baseurl, model_name="llama2-7b-chat", task_type="gpu")
 
-    with open("llm/prompt/system.txt", 'r') as system_file:
-        system = system_file.read()
-
-    with open("llm/prompt/user.txt", 'r') as user_file:
-        user = user_file.read()
-
-    messages = [
-        {"role": "system", "content": system},
-        {"role": "user", "content": user}
-    ]
-
-    with open("llm/prompt/functions.json", 'r') as functions_file:
-        functions = json.load(functions_file)
-
     while KEYRead() != KEY4 and max_value < 180:
 
         img = CAMGet()
         LCDImage(img)
         VWWait()
-        response = req.get_completion(messages=messages, functions=functions, function_call="auto", query_interval=1)
-        message_dict = ast.literal_eval(response['desc'])
-        command_list = message_dict['choices'][0]['message']['content']
-        logger.info(command_list)
 
+        dist = PSDGet(1)
         [res, max_col, max_value] = red_detector(img)
+        status = f"dist={dist}, res={res}, max_col={max_col}"
+        command=LLMQuery(req=req, status=status)
+        arguments = json.loads(command["arguments"])
+        if command["name"] == "VWTurn":
+            VWTurn(arguments["angle"], arguments["ang_speed"])
+            VWWait()
+        elif command["name"] == "VWStraight":
+            VWStraight(arguments["dist"], arguments["lin_speed"])
+            VWWait()
+        else:
+            logger.info("No command found")
 
-        if not res:  # no red pixels
-            VWTurn(5, 50)
-            VWWait()
-        elif max_col < QVGA_Y / 3:  # found red, turn left
-            VWTurn(5, 50)
-        elif max_col > 2 * QVGA_Y / 3:  # found red, turn right
-            VWTurn(-5, 50)
-        elif PSDGet(1) > SAFE:  # check if front is clear
-            VWStraight(50, 100)
-            VWWait()
+
+
+        # if not res:  # no red pixels
+        #     VWTurn(5, 50)
+        #     VWWait()
+        # elif max_col < QVGA_Y / 3:  # found red, turn left
+        #     VWTurn(5, 50)
+        # elif max_col > 2 * QVGA_Y / 3:  # found red, turn right
+        #     VWTurn(-5, 50)
+        # elif PSDGet(1) > SAFE:  # check if front is clear
+        #     VWStraight(50, 100)
+        #     VWWait()
 
     VWSetSpeed(0, 0)
 
--- src/llm/sample_request.py
+++ src/llm/sample_request.py
@@ -2,6 +2,7 @@ import logging
 from llm_request import LLMRequest
 import json
 import ast
+from langchain_community.llms import LlamaCpp
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -24,8 +25,10 @@ if __name__ == '__main__':
     ]
 
     with open("prompt/functions.json", 'r') as functions_file:
-        functions = json.load(functions_file)
+        tools = json.load(functions_file)
 
-    response = req.get_completion(messages=messages, functions=functions, function_call="auto")
+    response = req.get_completion(messages=messages, tools=tools, tool_choice="auto")
+
+    logger.info(response)
     message_dict = ast.literal_eval(response['desc'])
     logger.info(message_dict['choices'][0]['message']['content'])
