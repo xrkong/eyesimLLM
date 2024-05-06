5be783965a1fb5d7c99b1eb8c6fc629bbc1a365f
moebuta <wenxiao.zhang@research.uwa.edu.au>
2024-05-06 18:03:12 +0800

    update object-oriented coding style

---
+++ src/scripts/robot/eyebot.py
@@ -0,0 +1,59 @@
+from eye import *
+import time
+import threading
+
+class EyeBot:
+    def __init__(self, speed, angspeed):
+        self.speed = speed
+        self.angspeed = angspeed
+        self.safety_event = threading.Event()
+        self.llm_request_event = threading.Event()
+        self.psd_sensor_values = []
+
+    def move(self, speed, angspeed):
+        self.update_state(speed, angspeed)
+
+    def stop(self):
+        self.update_state(0, 0)
+
+    def update_state(self, speed, angspeed):
+        self.speed = speed
+        self.angspeed = angspeed
+        VWSetSpeed(self.speed, self.angspeed)
+
+    def LLM_control(self):
+        while True:
+            # trigger a LLM request
+            if self.llm_request_event.is_set():
+                # TODO: implement LLM control
+                # 1. read status data from logs
+                # 2. retrieve docs for planning
+                # 3. organise prompt
+                # 4. send prompt to LLM
+                self.llm_request_event.clear()
+            if self.safety_event.is_set():
+                # TODO: Triggering safety methods
+                self.stop()
+                self.safety_event.clear()
+                self.llm_request_event.set()
+            time.sleep(1)
+
+    def user_query(self):
+        # TODO: implement user query
+        # 1. a prompt window to accept user query
+        # 2. trigger request_event
+        # 3. send user query to LLM
+        # 4. display/record the response
+        pass
+
+    def run(self):
+        llm_control_thread = threading.Thread(target=self.LLM_control)
+        llm_control_thread.start()
+        user_query_thread = threading.Thread(target=self.user_query)
+        user_query_thread.start()
+        while True:
+            # TODO: implement safety mechanism
+            self.psd_sensor_values = [PSDGet(i) for i in range(1, 7)]
+            if any(value < 100 for value in self.psd_sensor_values):
+                self.llm_request_event.set()
+            time.sleep(1)
---
+++ src/scripts/run.py
@@ -0,0 +1,5 @@
+from scripts.robot.eyebot import EyeBot
+
+if __name__ == '__main__':
+    robot = EyeBot(speed=0, angspeed=0)
+    robot.run()
