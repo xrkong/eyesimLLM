9bd697252b56ba77d6a07138bfe2a5a4001e2a75
moebuta <wenxiao.zhang@research.uwa.edu.au>
2024-05-06 18:02:32 +0800

    fix space issue

--- start.bash
+++ start.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 export EYESIM_MAP='worlds/aquatic/mountain.wld'
-
 docker compose up -d
 docker exec -u 0 eyesim sed -i '/world/c\world '${EYESIM_MAP} /opt/eyesim/eyesimX/default.sim 
 docker exec eyesim eyesim
