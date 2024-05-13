5cc09ee1499083c3074467af629dcfa84f7c946b
moebuta <wenxiao.zhang@research.uwa.edu.au>
2024-05-13 17:39:53 +0800

    add neo4j

---
+++ database/README.md
@@ -0,0 +1,10 @@
+# Neo4J
+
+The `database` directory contains the scripts and configuration files
+used to control and configure Neo4J.
+
+```bash
+docker build -t neo4j .
+
+docker compose up
+```
--- src/evaluation/data_collection.py
+++ src/evaluation/data_collection.py
@@ -4,20 +4,41 @@ import pandas as pd
 
 from src.utils.constant import DATA_DIR
 
+import csv
+
 
 class DataCollection:
     def __init__(self):
         pass
 
-    def save_data(self, task_name:str, current_state: dict):
+    # def save_data(self, task_name: str, current_state: dict):
+    #     file_path = f'{DATA_DIR}/{task_name}.csv'
+    #     if os.path.exists(file_path):
+    #         df = pd.read_csv(file_path)
+    #         df = df.append(current_state, ignore_index=True)
+    #     else:
+    #         df = pd.DataFrame(current_state, index=[0])
+    #     df.to_csv(file_path, index=False, mode='a', header=not os.path.exists(file_path))
+    #
+    def save_data(self, task_name: str, current_state: dict):
         file_path = f'{DATA_DIR}/{task_name}.csv'
-        previous_state = self.load_data(file_path)
-        df = pd.concat([previous_state, pd.DataFrame(current_state)], ignore_index=True)
-        df.to_csv(file_path, index=False)
+        fieldnames = current_state.keys()
+
+        with open(file_path, mode='a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            # Write headers only if the file is empty
+            if file.tell() == 0:
+                writer.writeheader()
+
+            writer.writerow(current_state)
+
 
     def load_data(self, file_path: str):
         if not os.path.exists(file_path):
             return pd.DataFrame(columns=["speed", "angspeed", "img", "psd_sensor_values", "timestamp"])
         else:
             return pd.read_csv(file_path)
+
+
 
