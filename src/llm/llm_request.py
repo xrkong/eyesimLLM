cfadc2f25d288a2e230281787d2f8b201e2522a7
MoeBuTa <wenxiao.zhang@research.uwa.edu.au>
2024-05-16 20:08:22 +0800

    refactor llm requests and system prompts, add template for executing actions generated by LLMs (#25)

--- src/llm/llm_request.py
+++ src/llm/llm_request.py
@@ -3,97 +3,78 @@ import logging
 import os
 
 import httpx
-from constant import LLM_MODEL_DIR, PROMPT_DIR
 from dotenv import find_dotenv, load_dotenv
 from llama_cpp import Llama, LlamaGrammar
-from llm_call.llama_request import LLAMARequest
 from openai import OpenAI
 from tqdm import tqdm
+
+from src.llm.llama_request import LLAMARequest
+from src.utils.constant import LLM_MODEL_DIR, PROMPT_DIR
 
 _ = load_dotenv(find_dotenv())
 
 class LLMRequest:
-    def __init__(self):
+    def __init__(self, system_prompt: str, model_name: str='gpt-4o'):
         self.logger = logging.getLogger(__name__)
-        self.system = None
-        self.user = None
         self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-        self.llm = None
-        self.init_request()
+        self.model_name = model_name
+        self.system_prompt = system_prompt
 
-
-    def init_local_llama(self):
-        return Llama(
-            model_path=F"{LLM_MODEL_DIR}/llama2/llama-2-7b-chat.Q4_K_M.gguf",
-            n_gpu_layers=0,
-            embedding=True,
-            n_ctx=4096,
-        )
-
-    def init_request(self):
-        with open(f"{PROMPT_DIR}/system.txt", 'r') as system_file:
-            self.system = system_file.read()
-
-        with open(f"{PROMPT_DIR}/user.txt", 'r') as user_file:
-            self.user = user_file.read()
-
-    def query(self, query_type: str, model_name: str, user: str):
-        if query_type == "openai":
-            return self.openai_query(user=user, model_name=model_name)
-        return self.llama_query(user=user, model_name=model_name)
-
-
-    def openai_query(self, user: str, model_name: str='gpt-4'):
+    def openai_query(self, prompt: str):
+        """
+        Query OpenAI API for ChatCompletion
+        """
         messages = [
-            {"role": "system", "content": self.system},
-            {"role": "user", "content": user}
+            {"role": "system", "content": self.system_prompt},
+            {"role": "user", "content": prompt}
         ]
         try:
             response = self.client.chat.completions.create(
-                model=model_name,
+                model=self.model_name,
                 messages=messages,
                 response_format={"type":"json_object"}
             )
-            self.logger.info(response)
-            # command = self.response_handler(response=response)
-            # self.logger.info(command)
-            # return command.name, json.loads(command.arguments)
+            self.logger.info(f'{self.model_name} response: {response}')
+            return response.choices[0].message.content
         except Exception as e:
             print("Unable to generate ChatCompletion response")
             print(f"Exception: {e}")
             return e
 
-    def llama_query(self, user: str, model_name: str='Meta-Llama-3-8B-Instruct'):
-        messages = [
-            {"role": "system", "content": self.system},
-            {"role": "user", "content": user}
-        ]
-        req = LLAMARequest(model_name=model_name)
-        response = req.create_chat_completion(messages)['desc']
-        self.logger.info(response)
-
-        # command = self.response_handler(model_type=req.model_type, response=response)
-        # command = {"name": command["name"], "arguments": command["arguments"]}
-        # self.logger.info(command)
-        # return command['name'], (command['arguments'])
-
-    # def response_handler(self, response: Any, model_type="gpt"):
-    #     if model_type == HF_LLAMA:
-    #         return json.loads(
-    #             json.loads(response['desc'])[0]["generated_text"].split("\n\n")[-1])
-    #     if model_type == MT_LLAMA:
-    #         return json.loads(json.loads(response['desc'])["choices"][0]["message"]["content"])
-    #     return response.choices[0].message.tool_calls[0].function
-
-    def local_llama(self, user: str):
-        if self.llm is None:
-            self.llm = self.init_local_llama()
-
-        messages = [
-            {"role": "system", "content": self.system},
-            {"role": "user", "content": user}
-        ]
-        grammar_text = httpx.get(
-            "https://raw.githubusercontent.com/ggerganov/llama.cpp/master/grammars/json_arr.gbnf").text
-        grammar = LlamaGrammar.from_string(grammar_text)
-        return self.llm.create_chat_completion(messages=messages, grammar=grammar, max_tokens=-1)
+    # TODO: Legacy code for querying Open-source LLMs may be removed later
+    # def init_local_llama(self):
+    #     return Llama(
+    #         model_path=F"{LLM_MODEL_DIR}/llama2/llama-2-7b-chat.Q4_K_M.gguf",
+    #         n_gpu_layers=0,
+    #         embedding=True,
+    #         n_ctx=4096,
+    #     )
+    #
+    #
+    #
+    # def query(self, query_type: str, model_name: str, user: str):
+    #     if query_type == "openai":
+    #         return self.openai_query(user=user, model_name=model_name)
+    #     return self.llama_query(user=user, model_name=model_name)
+    #
+    # def llama_query(self, user: str, model_name: str='Meta-Llama-3-8B-Instruct'):
+    #     messages = [
+    #         {"role": "system", "content": self.system},
+    #         {"role": "user", "content": user}
+    #     ]
+    #     req = LLAMARequest(model_name=model_name)
+    #     response = req.create_chat_completion(messages)['desc']
+    #     self.logger.info(response)
+    #
+    # def local_llama(self, user: str):
+    #     if self.llm is None:
+    #         self.llm = self.init_local_llama()
+    #
+    #     messages = [
+    #         {"role": "system", "content": self.system},
+    #         {"role": "user", "content": user}
+    #     ]
+    #     grammar_text = httpx.get(
+    #         "https://raw.githubusercontent.com/ggerganov/llama.cpp/master/grammars/json_arr.gbnf").text
+    #     grammar = LlamaGrammar.from_string(grammar_text)
+    #     return self.llm.create_chat_completion(messages=messages, grammar=grammar, max_tokens=-1)
--- src/robot/eyebot_llm.py
+++ src/robot/eyebot_llm.py
@@ -1,3 +1,5 @@
+from src.llm.llm_request import LLMRequest
+from src.llm.prompt import control_prompt, system_prompt
 from src.robot import *
 
 
@@ -5,34 +7,63 @@ class EyebotLLM(EyebotBase):
     def __init__(self, task_name:str, speed: int = 0, angspeed: int = 0):
         super().__init__(task_name, speed, angspeed)
         self.llm_request_event = threading.Event()
+        self.llm_control_event = threading.Event()
         self.manual_event = threading.Event()
         self.logger = logging.getLogger(__name__)
+
+    def execute_action_list(self, action_list: List[Dict]):
+        """
+        execute the list of actions
+        """
+        while len(action_list)>0 and self.llm_control_event.is_set():
+            action = action_list.pop(0)
+            self.update_state(action["speed"], action["angspeed"])
+            time.sleep(action["duration"])
 
     def control(self):
         """
         control the robot's movement based on the events
         """
+        llm = LLMRequest(system_prompt=system_prompt(), model_name='gpt-4o')
+
         while True:
             if self.safety_event.is_set():
                 self.logger.info("Safety event triggered!")
+                self.safety_event.clear()
                 # TODO: Triggering safety methods
                 self.stop()
-                self.safety_event.clear()
+
                 # self.llm_request_event.set()
             # trigger a LLM request
             if self.llm_request_event.is_set():
-                self.logger.info("LLM request triggered!")
-                self.move(50, 20)
+                self.logger.info("LLM request triggered! ")
+                self.llm_request_event.clear()
+                # self.move(50, 20)
                 # TODO: implement LLM control
                 # 1. read status data from logs
                 # 2. retrieve docs for planning
                 # 3. organise prompt
+                prompt = control_prompt(current_state=self.to_dict(), experience_states=[], user_query="")
                 # 4. send prompt to LLM
-                self.llm_request_event.clear()
+                # TODO: uncomment the following line to use openai API
+                # action_list = llm.openai_query(prompt=prompt)
+                # TODO: the following line is used for template testing
+                action_list = [{"speed": 50, "angspeed": 20, "duration": 5},
+                               {"speed": 30, "angspeed": -20, "duration": 5},
+                               {"speed": 40, "angspeed": 10, "duration": 5}]
+
+                # reset the control event to discard the previous LLM control command and accept new control command
+                self.llm_control_event.clear()
+                self.llm_control_event.set()
+                # execute the action list
+                action_execution_thread = threading.Thread(target=self.execute_action_list, args=(action_list,))
+                action_execution_thread.start()
+
             if self.manual_event.is_set() and not self.safety_event.is_set():
                 #self.logger.info("Manual control! "+ str(self.safety_event.is_set()))
                 self.manual_control()
                 self.llm_request_event.clear()
+            # TODO: need to comment out this line in the final version
             time.sleep(CONTROL_EVENT_CHECK_FREQUENCY)
 
     def user_query(self):
@@ -44,6 +75,7 @@ class EyebotLLM(EyebotBase):
         # 2. trigger request_event
         # 3. send user query to LLM
         # 4. display/record the response
+
         pass
 
     def run(self, drive_mode=None):
