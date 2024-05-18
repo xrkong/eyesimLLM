1e18739ce4ee987212aeec31804c0f2fbf2e5f45
moebuta <wenxiao.zhang@research.uwa.edu.au>
2024-05-18 18:27:32 +0800

    fix bugs

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == '__main__':
-    robot = EyebotLLM(task_name="llm_test", speed=0, angspeed=0)
+    robot = EyebotLLM(task_name="llm")
     # robot = EyebotManual(task_name="manual_test", speed=0, angspeed=0)
     # robot  = EyebotLawnmower(task_name="lawnmower_test", speed=0, angspeed=0)
     # robot.run(drive_mode="manual")
