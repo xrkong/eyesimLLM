33b7c5400872f046e9e09161359c989419af6562
MoeBuTa <wenxiao.zhang@research.uwa.edu.au>
2024-05-18 15:50:12 +0800

    Rag/lidar img (#26)
    * initially implement lidar image creating and saving
    
    * fix formatting

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,8 @@
+from typing import List, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 from eye import *
 from PIL import Image
 
@@ -8,3 +13,25 @@ def cam2image(image):
     """
     image = Image.frombytes('RGB', (QVGA_X, QVGA_Y), image)
     return image
+
+
+def lidar2image(scan: List[int], experiment_time: str, save_path: str):
+    """
+    Plots a line plot for the given LiDAR data and returns the figure object.
+
+    Parameters:
+    lidar_data (list): The LiDAR data containing 360 values.
+
+    Returns:
+    fig (matplotlib.figure.Figure): The figure object containing the plot.
+    """
+    # Create an array for degrees (0 to 359)
+    degrees = np.arange(360)
+
+    # Create the line plot
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.lineplot(x=degrees, y=scan, ax=ax)
+    ax.set_title(f'LiDAR Data Line Plot for Each Degree at Time={experiment_time}')
+    ax.set_xlabel('Degree')
+    ax.set_ylabel('Distance')
+    fig.savefig(save_path)
